3dce7b8a9dd44280a3c067ba203cbb54a7ab32c4
anthonymelson <anthonycmelson@gmail.com>
2019-06-22 16:48:52 -0500

    Add files via upload

---
+++ new/appengine_config.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 20 13:57:12 2019
+
+@author: Anthony Melson
+"""
+
+from google.appengine.ext import vendor
+
+# Add any libraries installed in the "lib" folder.
+vendor.add('lib')
